c43cac549ca172110b881c21de571d03d035e3e0
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-07 01:07:45 +0900

    테이블 rename

--- src/main/java/me/springstudy/jpastudy/user/User.java
+++ src/main/java/me/springstudy/jpastudy/user/User.java
@@ -8,9 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name = "TB_USER")
 public class User {
 
 	@Id
