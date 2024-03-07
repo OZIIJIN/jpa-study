d01f2bc6a27207f9f2fb21e25c07b31dab54b81d
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-07 16:42:27 +0900

    UserChannelRepository 생성

---
+++ src/main/java/me/springstudy/jpastudy/userchannel/UserChannelRepository.java
@@ -0,0 +1,22 @@
+package me.springstudy.jpastudy.userchannel;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import me.springstudy.jpastudy.channel.Channel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserChannelRepository {
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	public UserChannel insertUserChannel (UserChannel userchannel) {
+		entityManager.persist(userchannel);
+		return userchannel;
+	}
+
+	public Channel selectUserChannel (Long id) {
+		return entityManager.find(Channel.class, id);
+	}
+}
